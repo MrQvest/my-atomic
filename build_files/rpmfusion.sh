196a26d819578a93a14759784ff9a159473b1abc
MrQvest <104365438+MrQvest@users.noreply.github.com>
2025-06-14 22:55:27 -0300

    We do NOT update

--- build_files/rpmfusion.sh
+++ build_files/rpmfusion.sh
@@ -13,9 +13,6 @@ dnf5 -y install "${RPMFusion[@]}"
 # Swap ffmpeg-free for ffmpeg
 dnf5 -y swap ffmpeg-free ffmpeg --allowerasing
 
-# Install additional codec
-dnf5 -y update @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
-
 # Play a DVD
 dnf5 -y install rpmfusion-free-release-tainted
 dnf5 -y install libdvdcss
