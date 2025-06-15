79a8712ef5e20d1f043f1176e0e9f9dbf5ab7df7
MrQvest <104365438+MrQvest@users.noreply.github.com>
2025-06-14 22:36:30 -0300

    swap ffmpeg-free for ffmpeg

--- build_files/rpmfusion.sh
+++ build_files/rpmfusion.sh
@@ -3,10 +3,12 @@
 set -ouex pipefail
 
 # Enable RPMFusion
-
 RPMFusion=(
   https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
   https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 )
 
 dnf5 -y install "${RPMFusion[@]}"
+
+# Swap ffmpeg-free for ffmpeg
+dnf5 -y swap ffmpeg-free ffmpeg --allowerasing
