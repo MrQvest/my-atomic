a60b14b5a95595bdb66a3d81f03c05cd63455761
MrQvest <104365438+MrQvest@users.noreply.github.com>
2025-06-14 22:20:51 -0300

    Create RPMFusion script

---
+++ build_files/rpmfusion.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+# Enable RPMFusion
+
+RPMFusion=(
+  https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+  https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+)
+
+dnf5 -y install "${RPMFusion[@]}"
